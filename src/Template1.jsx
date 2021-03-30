bd244042f5feccdba97006ffe9cb220d3018a4eb
Akhila <akhila@neoito.com>
2021-03-31 02:13:05 +0530

    modify template1 styles - change the position of social links - wrap everything in another container - group basic details and contact details

--- src/Template1.jsx
+++ src/Template1.jsx
@@ -8,31 +8,37 @@ import "./template1.css";
 function Template1() {
 	return (
 		<div className="template1">
-			<img
-				className="profile-img"
-				src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
-				alt="profile"
-			/>
-			<div className="social-links">
-				<img className="social-logo" src={linkedin} alt="facebook logo" />
-				<img className="social-logo" src={facebook} alt="facebook logo" />
-				<img className="social-logo" src={twitter} alt="twitter logo" />
-				<img className="social-logo" src={instagram} alt="instagram logo" />
-			</div>
+			<div className="signature">
+				<img
+					className="profile-img"
+					src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
+					alt="profile"
+				/>
+				<div className="details">
+					<div className="basic-details">
+						<div className="full-name">
+							<h5>First Name</h5>
+							<h5>Last Name</h5>
+						</div>
+						<p>Job Title</p>
+						<p>Department | Company</p>
+					</div>
+					<div className="contact-details">
+						<p>Phone Number | Mobile Number</p>
+						<p>aaa@aaa.com</p>
+						<p>www.aaa.com</p>
+						<p>line1, line2, line3, line4</p>
+					</div>
+				</div>
 
-			<div>
-				<h5>First Name</h5>
-				<h5>Last Name</h5>
+				<hr />
+				<div className="social-links">
+					<img className="social-logo" src={linkedin} alt="facebook logo" />
+					<img className="social-logo" src={facebook} alt="facebook logo" />
+					<img className="social-logo" src={twitter} alt="twitter logo" />
+					<img className="social-logo" src={instagram} alt="instagram logo" />
+				</div>
 			</div>
-			<p>Job Title</p>
-			<p>Department</p>
-			<p>Company</p>
-
-			<hr />
-			<p>Phone Number | Mobile Number</p>
-			<p>aaa@aaa.com</p>
-			<p>www.aaa.com</p>
-			<p>line1, line2, line3, line4</p>
 		</div>
 	);
 }
