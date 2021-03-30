23fa5c122e583f9376e234f95fbe82b710c06395
Akhila <akhila@neoito.com>
2021-03-30 16:32:12 +0530

    add labels, placeholders and some styles

--- src/Form.jsx
+++ src/Form.jsx
@@ -10,35 +10,51 @@ function Form() {
 	return (
 		<form className="form" onSubmit={handleSubmit(onSubmit)}>
 			<label>First Name</label>
-			<input name="firstName" ref={register} />
+			<input name="firstName" ref={register} placeholder="John" />
 			<label>Last Name</label>
-			<input name="lastName" ref={register} />
+			<input name="lastName" ref={register} placeholder="Doe" />
 			<label>Job Title</label>
-			<input name="jobTitle" ref={register} />
+			<input name="jobTitle" ref={register} placeholder="Consultant" />
 			<label>Department</label>
-			<input name="department" ref={register} />
+			<input name="department" ref={register} placeholder="Business" />
 			<label>Company Name</label>
-			<input name="companyName" ref={register} />
+			<input name="companyName" ref={register} placeholder="Neoito" />
 			<label>Office Number</label>
-			<input name="phoneNum" ref={register} />
+			<input name="phoneNum" ref={register} placeholder="111 222 333" />
 			<label>Mobile Number</label>
-			<input name="mobileNum" ref={register} />
+			<input name="mobileNum" ref={register} placeholder="11 222 333" />
 			<label>Website URL</label>
-			<input name="siteUrl" ref={register} />
+			<input name="siteUrl" ref={register} placeholder="john@doe.com" />
 			<label>Email Address</label>
-			<input name="email" ref={register} />
+			<input name="email" ref={register} placeholder="www.neoito.com" />
 			<label>Address</label>
-			<input name="addLine1" ref={register} />
-			<input name="addLine2" ref={register} />
-			<input name="addLine3" ref={register} />
-			<input name="addLine4" ref={register} />
+			<input name="addLine1" ref={register} placeholder="Address Line 1" />
+			<input name="addLine2" ref={register} placeholder="Address Line 2" />
+			<input name="addLine3" ref={register} placeholder="Address Line 3" />
+			<input name="addLine4" ref={register} placeholder="Address Line 4" />
 			<label>Social Links</label>
-			<input name="linkedin" ref={register} />
-			<input name="facebook" ref={register} />
-			<input name="twitter" ref={register} />
-			<input name="instagram" ref={register} />
+			<input
+				name="linkedin"
+				ref={register}
+				placeholder="https://www.linkedin.com"
+			/>
+			<input
+				name="facebook"
+				ref={register}
+				placeholder="https://www.facebook.com"
+			/>
+			<input
+				name="twitter"
+				ref={register}
+				placeholder="https://www.twitter.com"
+			/>
+			<input
+				name="instagram"
+				ref={register}
+				placeholder="https://www.instagram.com"
+			/>
 
-			<input type="submit" />
+			<input className="submit-btn" type="submit" />
 		</form>
 	);
 }
