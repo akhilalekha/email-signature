317d8fc45ac97ba2fbdc049cdbe3f18ce79de374
Akhila <akhila@neoito.com>
2021-03-31 16:43:33 +0530

    modify template styles - add contact icons - use grid for contact details - use hr vertically, delete margin - change font-size for p tags and fullname

--- src/Template1.jsx
+++ src/Template1.jsx
@@ -3,6 +3,10 @@ import facebook from "./facebook_circle.svg";
 import linkedin from "./linkedin_circle.svg";
 import twitter from "./twitter_circle.svg";
 import instagram from "./instagram_circle.svg";
+import pin from "./pin.svg";
+import phone from "./phone-alt-solid.svg";
+import mail from "./envelope-solid.svg";
+import link from "./chain.svg";
 import "./template1.css";
 
 function Template1() {
@@ -16,17 +20,22 @@ function Template1() {
 						alt="profile"
 					/>
 					<div className="basic-details">
-						<div className="full-name">
-							<h5>First Name</h5>
-							<h5>Last Name</h5>
-						</div>
+						<p className="fullname">First Name Last Name</p>
 						<p>Job Title</p>
 						<p>Department | Company</p>
 					</div>
+					<hr className="vr" />
 					<div className="contact-details">
+						<img className="contact-icon" src={phone} alt="phone icon" />
 						<p>Phone Number | Mobile Number</p>
+
+						<img className="contact-icon" src={mail} alt="mail icon" />
 						<p>aaa@aaa.com</p>
+
+						<img className="contact-icon" src={link} alt="url icon" />
 						<p>www.aaa.com</p>
+
+						<img className="contact-icon" src={pin} alt="location icon" />
 						<p>line1, line2, line3, line4</p>
 					</div>
 				</div>
