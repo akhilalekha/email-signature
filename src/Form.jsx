930bb79a1c7f035f58ebcaef1425da9ce0efcbde
Akhila <akhila@neoito.com>
2021-03-31 14:56:08 +0530

    modify form styles - change background colour - change label, input, placeholder font colours, submit button colour

--- src/Form.jsx
+++ src/Form.jsx
@@ -29,68 +29,70 @@ function Form() {
 	const onSubmit = data => console.log(data);
 
 	return (
-		<form className="form" onSubmit={handleSubmit(onSubmit)}>
-			<h4>Enter your signature details</h4>
-			<label htmlFor="firstName">First Name</label>
-			<input name="firstName" ref={register} placeholder="John" />
+		<div className="form-container">
+			<form className="form" onSubmit={handleSubmit(onSubmit)}>
+				<h4>Enter your signature details</h4>
+				<label htmlFor="firstName">First Name</label>
+				<input name="firstName" ref={register} placeholder="John" />
 
-			<label htmlFor="lastName">Last Name</label>
-			<input name="lastName" ref={register} placeholder="Doe" />
+				<label htmlFor="lastName">Last Name</label>
+				<input name="lastName" ref={register} placeholder="Doe" />
 
-			<label htmlFor="jobTitle">Job Title</label>
-			<input name="jobTitle" ref={register} placeholder="Consultant" />
+				<label htmlFor="jobTitle">Job Title</label>
+				<input name="jobTitle" ref={register} placeholder="Consultant" />
 
-			<label htmlFor="department">Department</label>
-			<input name="department" ref={register} placeholder="Business" />
+				<label htmlFor="department">Department</label>
+				<input name="department" ref={register} placeholder="Business" />
 
-			<label htmlFor="companyName">Company Name</label>
-			<input name="companyName" ref={register} placeholder="Neoito" />
+				<label htmlFor="companyName">Company Name</label>
+				<input name="companyName" ref={register} placeholder="Neoito" />
 
-			<label htmlFor="phoneNum">Office Number</label>
-			<input name="phoneNum" ref={register} placeholder="111 222 333" />
+				<label htmlFor="phoneNum">Office Number</label>
+				<input name="phoneNum" ref={register} placeholder="111 222 333" />
 
-			<label htmlFor="mobileNum">Mobile Number</label>
-			<input name="mobileNum" ref={register} placeholder="11 222 333" />
+				<label htmlFor="mobileNum">Mobile Number</label>
+				<input name="mobileNum" ref={register} placeholder="11 222 333" />
 
-			<label htmlFor="siteUrl">Website URL</label>
-			<input name="siteUrl" ref={register} placeholder="john@doe.com" />
+				<label htmlFor="siteUrl">Website URL</label>
+				<input name="siteUrl" ref={register} placeholder="john@doe.com" />
 
-			<label htmlFor="email">Email Address</label>
-			<input
-				name="email"
-				ref={register}
-				placeholder="www.neoito.com"
-				type="email"
-			/>
-			<label>Address</label>
-			<input name="addLine1" ref={register} placeholder="Address Line 1" />
-			<input name="addLine2" ref={register} placeholder="Address Line 2" />
-			<input name="addLine3" ref={register} placeholder="Address Line 3" />
-			<input name="addLine4" ref={register} placeholder="Address Line 4" />
-			<label>Social Links</label>
-			<input
-				name="linkedin"
-				ref={register}
-				placeholder="https://www.linkedin.com"
-			/>
-			<input
-				name="facebook"
-				ref={register}
-				placeholder="https://www.facebook.com"
-			/>
-			<input
-				name="twitter"
-				ref={register}
-				placeholder="https://www.twitter.com"
-			/>
-			<input
-				name="instagram"
-				ref={register}
-				placeholder="https://www.instagram.com"
-			/>
+				<label htmlFor="email">Email Address</label>
+				<input
+					name="email"
+					ref={register}
+					placeholder="www.neoito.com"
+					type="email"
+				/>
+				<label>Address</label>
+				<input name="addLine1" ref={register} placeholder="Address Line 1" />
+				<input name="addLine2" ref={register} placeholder="Address Line 2" />
+				<input name="addLine3" ref={register} placeholder="Address Line 3" />
+				<input name="addLine4" ref={register} placeholder="Address Line 4" />
+				<label>Social Links</label>
+				<input
+					name="linkedin"
+					ref={register}
+					placeholder="https://www.linkedin.com"
+				/>
+				<input
+					name="facebook"
+					ref={register}
+					placeholder="https://www.facebook.com"
+				/>
+				<input
+					name="twitter"
+					ref={register}
+					placeholder="https://www.twitter.com"
+				/>
+				<input
+					name="instagram"
+					ref={register}
+					placeholder="https://www.instagram.com"
+				/>
 
-			<input className="submit-btn" type="submit" />
-		</form>
+				<input className="submit-btn" type="submit" />
+			</form>
+		</div>
 	);
 }
 
